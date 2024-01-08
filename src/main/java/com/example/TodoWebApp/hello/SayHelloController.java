a6af73f59262d6136a14d3562cd6df07325e8dee
Ragamaliga <mragamaliga@gmail.com>
2024-01-08 13:43:14 +0530

    version 1 without jsp

--- src/main/java/com/example/TodoWebApp/hello/SayHelloController.java
+++ src/main/java/com/example/TodoWebApp/hello/SayHelloController.java
@@ -1,4 +1,15 @@
 package com.example.TodoWebApp.hello;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class SayHelloController {
+
+@RequestMapping("sayHello.jsp")
+@ResponseBody
+ public String sayHello(){
+     return "hello web_API";
+ }
 }
